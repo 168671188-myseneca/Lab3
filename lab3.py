ffae63dc691d2fe1ff51d13da6e2b9a094f54f40
168671188-myseneca <dmorales5@myseneca.ca>
2024-02-22 02:07:17 +0100

    Lab3 Update

--- lab3.py
+++ lab3.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python 3
 
+def helloWorld():
+    print("Hello world!")
+
+helloWorld()
+
 # Write a text file with your respected name
 f = open("yourName.txt", "w") 
 f.write("David Morales")
